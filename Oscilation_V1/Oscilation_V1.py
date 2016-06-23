66e084f174986cb8d42e4fdbe99d21cf63e5a67b
luka moric <lumoric@gmail.com>
2016-06-23 16:17:23 +1200

    vc

--- Oscilation_V1/Oscilation_V1.py
+++ Oscilation_V1/Oscilation_V1.py
@@ -39,8 +39,8 @@ class damped_SDOF:
         self.m  = m_init
         self.damp_ratio = damp_ratio
 
-        self.a = self.x0*self.omega_damp()
-        self.b = self.v0 + self.damp_ratio*self.omega_nat()*self.x0
+        self.Real = self.x0*self.omega_damp()
+        self.Imagin = self.v0 + self.damp_ratio*self.omega_nat()*self.x0
 
     def displ(self, t):
         return self.A()*np.power(np.e, -1.*(self.damp_ratio*self.omega_nat()*t))*np.sin(self.omega_damp()*t + self.phase())
@@ -58,10 +58,10 @@ class damped_SDOF:
         return self.omega_nat()*(np.sqrt(1.-np.square(self.damp_ratio)))
 
     def A(self):
-        return np.sqrt(np.square(self.b) + np.square(self.a))/self.omega_damp()
+        return np.sqrt(np.square(self.Imagin) + np.square(self.Real))/self.omega_damp()
     
     def phase(self):
-        return np.arctan(self.a/self.b)
+        return np.arctan(self.Real/self.Imagin)
     
 class Harmonic_exc():
     def __init__(self, x_init, v_init, k_init, 
@@ -75,8 +75,8 @@ class Harmonic_exc():
         self.F_freq = F_freq
         self.f0 = self.F/self.m
 
-        self.a = self.x0*self.omega_damp()
-        self.b = self.v0 + self.damp_ratio*self.omega_nat()*self.x0
+        self.Real = self.x0*self.omega_damp()
+        self.Imagin = self.v0 + self.damp_ratio*self.omega_nat()*self.x0
 
     def displ(self, t):
         g = self.f0/(np.square(self.omega_nat())-np.square(self.F_freq))
@@ -98,14 +98,67 @@ class Harmonic_exc():
         return self.omega_nat()*(np.sqrt(1.-np.square(self.damp_ratio)))
 
     def A(self):
-        return np.sqrt(np.square(self.b) + np.square(self.a))/self.omega_damp()
+        return np.sqrt(np.square(self.Imagin) + np.square(self.Real))/self.omega_damp()
     
     def phase(self):
-        return np.arctan(self.a/self.b)
+        return np.arctan(self.Real/self.Imagin)
+
+
+class Harmonic_exc_DAMP():
+    def __init__(self, x_init, v_init, k_init, 
+                 m_init, damp_ratio, F, F_freq):
+        self.x0 = x_init
+        self.v0 = v_init
+        self.k  = k_init
+        self.m  = m_init
+        self.damp_ratio = damp_ratio
+        self.F = F
+        self.F_freq = F_freq
+        self.f0 = self.F/self.m
+
+        self.Real = self.x0*self.omega_damp()
+        self.Imagin = self.v0 + self.damp_ratio*self.omega_nat()*self.x0
+
+    def displ(self, t):
+        f = np.power(np.e, -self.damp_ratio*self.omega_nat()*t)
+        s = np.sin(self.omega_damp()*t + self.phase())
+        r = np.cos(self.F_freq*t - self.theta())
+        return self.A()*f*r + self.x0*r
+
+    def velocity(self, t):
+        return derivative(self.displ,t)
+
+    def acceleration(self, t):
+        return derivative(self.velocity,t)
+
+    def X(self):
+        b = np.square(np.square(self.omega_nat()) - np.square(self.F_freq))        
+        c = np.square(2*self.damp_ratio*self.omega_nat()*self.F_freq)
+        return self.f0/np.sqrt(b + c)
+
+    def theta(self):
+        b = np.square(self.omega_nat()) - np.square(self.F_freq)       
+        c = 2*self.damp_ratio*self.omega_nat()*self.F_freq
+        return np.arctan(c/b)
+
+    def omega_nat(self):
+        return np.sqrt(self.k/self.m)
+
+    def omega_damp(self):
+        return self.omega_nat()*(np.sqrt(1.-np.square(self.damp_ratio)))
+
+    def A(self):
+        return np.sqrt(np.square(self.Imagin) + np.square(self.Real))/self.omega_damp()
+    
+    def phase(self):
+        if self.Imagin != 0:
+            return np.arctan(self.Real/self.Imagin)
+        else:
+            return np.pi/2
 
 def __main__():
-    sdof = Harmonic_exc(.0, .0, 11., 10., .408, 2., 0.5)
-    t1 = np.arange(0.0, 20.0, .1)
+    sdof = Harmonic_exc_DAMP(.1, .0, 30., 10., .0, 200., 0.5)
+    t1 = np.arange(0.0, 500.0, .1)
     print sdof.velocity(t1)
     plt.plot(t1, sdof.displ(t1))
     plt.plot(t1, sdof.velocity(t1))
